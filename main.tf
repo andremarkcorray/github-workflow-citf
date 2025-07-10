ba1f7f6a9b7d80513406ebfb0d85e9d07185bbe4
andremarkcorray <andremarkcorray@gmail.com>
2025-07-10 21:29:34 +0800

    terraform fmt

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = "andre-sctp-ce10.tfstate"  #Change this
+    key    = "andre-sctp-ce10.tfstate" #Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix = "sctp-ce10-andre-tfci"  # Set your bucket name here
+  bucket_prefix = "sctp-ce10-andre-tfci" # Set your bucket name here
 }
