b6c6236f8cdb7fddf977b5f40b55796a0a660296
andremarkcorray <andremarkcorray@gmail.com>
2025-07-10 20:47:32 +0800

    Adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key    = "andre-sctp-ce10.tfstate"  #Change this
+    region = "ap-southeast-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket_prefix = "sctp-ce10-andre-tfci"  # Set your bucket name here
+}
